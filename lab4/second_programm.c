a02951ac3f29d85408b623c2d9ca3e1254c16967
Saffyan <99216012+Terentiyy@users.noreply.github.com>
2025-03-06 15:05:12 +0300

    Update second_programm.c

--- lab4/second_programm.c
+++ lab4/second_programm.c
@@ -1,96 +1,108 @@
-#include <stdio.h>
+#include <stdio.h>         //g++ prog2.cpp -L. -ldl -o main -Wl,-rpath -Wl,.
 #include <stdlib.h>
 #include <dlfcn.h>
+#include <vector>
 
-typedef enum{
-  first_realization,
-  second_realization,
-} contract;
+using namespace std;
 
-contract cont = first_realization;
-
-const char* lib_name_first = "./libs/First_lib.so";
-const char* lib_name_second = "./libs/Second_lib.so";
-
-float (*Square)(float, float) = NULL;
-int (*GCF)(int, int) = NULL;
-
-void *lib_chosen = NULL;
-
-void load_libs(contract contract){
-    const char* name;
-    switch (contract) {
-        case first_realization:{
-            name = lib_name_first;
-            break;
-        }
-        case second_realization:{
-            name = lib_name_second;
-            break;
-        }
+int main()
+{
+    void* h = NULL;
+    int (*GCF)(int A, int B);
+    int* (*Sort)(int* array);
+    int lib;
+    printf("0 for change libs, 1 for re1.cpp, 2 for re2.cpp, 3 for exit:\n");
+    scanf("%d", &lib);
+    while ((lib != 1)&&(lib != 2))
+    {
+        printf("Input error, try again:\n");
+        scanf("%d", &lib);
     }
-    lib_chosen = dlopen(name, RTLD_LAZY);
-    if (lib_chosen == NULL){
-        perror("dlopen");
-        exit(EXIT_FAILURE);
+    if (lib == 1)
+    {
+        h = dlopen("./libd1.so", RTLD_LAZY);
     }
-}
-
-void load_contract(){
-  load_libs(cont);
-  Square = dlsym(lib_chosen, "Square");
-  GCF = dlsym(lib_chosen, "GCF");
-}
-
-void change_contract(){
-  dlclose(lib_chosen);
-  switch (cont) {
-	case first_realization:{
-		cont = second_realization;
-		break;
-	}
-	case second_realization:{
-	  cont = first_realization;
-	  break;
-	}
-  }
-  load_contract();
-}
-
-int main(){
-    load_contract();
-    int value = 0;
-    while (scanf("%d", &value) != EOF){
-        switch (value) {
-            case 0:{
-                change_contract();
-                printf("contract has been changed\n");
-                switch (cont) {
-                case first_realization: {
-                    printf("contract is first_realization\n");
-                    break;
-                }
-                case second_realization:{
-                    printf("contract is second_realization\n");
-                }
+    if (lib == 2)
+    {
+        h = dlopen("./libd2.so", RTLD_LAZY);
+    }
+    GCF = (int(*)(int, int))dlsym(h, "GCF");
+    Sort = (int*(*)(int*))dlsym(h, "Sort");
+    unsigned command;
+    printf("1 for GCF, 2 for Sort:\n");
+    char c1 = '1', c2 = '1';
+    scanf("%d", &command);
+    while (1)
+    {
+        if (command == 0)
+        {
+            if (lib == 1)
+            {
+                dlclose(h);
+                h = dlopen("./libd2.so", RTLD_LAZY);
+                GCF = (int(*)(int, int))dlsym(h, "GCF");
+                Sort = (int*(*)(int*))dlsym(h, "Sort");
+                lib = 2;
+                printf("re1 changed on re2\n");
+                scanf("%d", &command);
+                continue;
             }
-            break;
-            }
-            case 1:{
-                int a, b;
-                if (scanf("%d %d", &a, &b) == 2){
-                    printf("%d\n", GCF(a, b));
-                }
-                break;
-            }
-            case 2:{
-                float a, b;
-                if (scanf("%f %f", &a, &b) == 2){
-                    printf("%f\n", Square(a,b));   
-                }
+            else
+            {
+                dlclose(h);
+                h = dlopen("./libd1.so", RTLD_LAZY);
+                GCF = (int(*)(int, int))dlsym(h, "GCF");
+                Sort = (int*(*)(int*))dlsym(h, "Sort");
+                lib = 1;
+                printf("re2 changed on re1\n");
+                scanf("%d", &command);
+                continue;
             }
         }
+        if (command == 1)
+        {
+            int a, b;
+            scanf("%d%c%d%c", &a, &c1, &b, &c2);
+            if ((a < 1)||(b < 1))
+            {
+                printf("Input error\n");
+            }
+            else
+            {
+                printf("%d\n", GCF(a, b));
+            }
+        }
+        if (command == 2)
+        {
+            int a;
+            char c = '1';
+            vector<int> v;
+            while (c != '\n')
+            {
+                scanf("%d%c", &a, &c);
+                v.push_back(a);
+            }
+            c = '1';
+            int* arr = new int[v.size() + 1];
+            arr[0] = v.size();
+            for (int i = 0; i < v.size(); ++i)
+            {
+                arr[i + 1] = v[i];
+            }
+            Sort(arr);
+            for (int i = 1; i < arr[0] + 1; ++i)
+            {
+                printf("%d ", arr[i]);
+            }
+            printf("\n");
+            delete [] arr;
+        }
+        if (command == 3)
+        {
+            break;
+        }
+        scanf("%d", &command);
     }
-    dlclose(lib_chosen);
+    dlclose(h);
     return 0;
 }
