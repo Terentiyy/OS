c9608b0806d55d9594e2d94756558dcfdda9b6cf
Saffyan <99216012+Terentiyy@users.noreply.github.com>
2025-03-06 14:59:08 +0300

    Update main.c

--- lab2/main.c
+++ lab2/main.c
@@ -1,121 +1,140 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <stddef.h>
+#include <pthread.h>
+#include <time.h>  
+
+#define RUN 32 
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+void insertionSort(int arr[], int left, int right) {
+    for (int i = left + 1; i <= right; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= left && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void merge(int arr[], int left, int mid, int right) {
+    int len1 = mid - left + 1, len2 = right - mid;
+    int *leftArr = (int *)malloc(len1 * sizeof(int));
+    int *rightArr = (int *)malloc(len2 * sizeof(int));
+
+    for (int i = 0; i < len1; i++)
+        leftArr[i] = arr[left + i];
+    for (int i = 0; i < len2; i++)
+        rightArr[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    while (i < len1 && j < len2) {
+        if (leftArr[i] <= rightArr[j]) {
+            arr[k] = leftArr[i];
+            i++;
+        } else {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < len1) {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < len2) {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
+
+    free(leftArr);
+    free(rightArr);
+}
+
+void timSort(int arr[], int n) {
+    for (int i = 0; i < n; i += RUN)
+        insertionSort(arr, i, (i + RUN - 1) < (n - 1) ? (i + RUN - 1) : (n - 1));
+
+    for (int size = RUN; size < n; size = 2 * size) {
+        for (int left = 0; left < n; left += 2 * size) {
+            int mid = left + size - 1;
+            int right = (left + 2 * size - 1) < (n - 1) ? (left + 2 * size - 1) : (n - 1);
+
+            if (mid < right)
+                merge(arr, left, mid, right);
+        }
+    }
+}
 
 typedef struct {
-    size_t row; size_t row_start; size_t row_end; size_t columns_count; double** massive; double gen_value;
-} GaussArgs;
+    int *arr;
+    int start;
+    int end;
+} ThreadData;
 
-void* func_for_threads(void* args){
-    GaussArgs *gauss_args = (GaussArgs *)args;
-    size_t row = gauss_args->row;
-    size_t row_start = gauss_args->row_start;
-    size_t row_end = gauss_args->row_end; 
-    size_t columns_count = gauss_args->columns_count;
-    double** massive = gauss_args->massive;
-    double gen_value = gauss_args->gen_value;
-    double to_replace = 0;
-    for (size_t sub_row = row_start; sub_row < row_end; sub_row++){
-        double value_under_gen = massive[sub_row][row];
-        double coff = value_under_gen/gen_value;
-        for (size_t col = row; col < columns_count; col++){
-            to_replace = massive[sub_row][col] - massive[row][col]*coff;
-            massive[sub_row][col] = to_replace;
-        }
-    }
-    pthread_exit(0);
+void *sortArray(void *arg) {
+    ThreadData *data = (ThreadData *)arg;
+    timSort(data->arr + data->start, data->end - data->start); 
+    pthread_exit(NULL);
 }
 
-//massive[строки][колонны]  massive[row][column]
-void Gauss_func(size_t columns_count, size_t rows_count, double** massive, size_t thread_count) {
-    pthread_t tid[thread_count];
-    size_t rows_per_thread = 1;
-    size_t remaining_rows = 0;
-    GaussArgs* args_array = malloc(thread_count * sizeof(GaussArgs));
-    size_t pthread_used = 0;
-    for (size_t row = 0; row < rows_count; row++) {
-        double gen_value = massive[row][row];
-        if (gen_value != 0.0) {
-            if (row < rows_count - 1) {
-                for (size_t i = 0; i < thread_count; i++) {
-                    if (thread_count < (rows_count - 1) - row ){
-                        rows_per_thread = ((rows_count- 1) - row) / thread_count;
-                        remaining_rows = (rows_count - 1 - row) % thread_count;
-                    }
-                    size_t row_start = (i * rows_per_thread) + 1 + row;
-                    size_t row_end = row_start + rows_per_thread;
-                    if (i == thread_count - 1) {
-                        row_end += remaining_rows;
-                    }
-                    if (row_end <= rows_count){
-                        args_array[i] = (GaussArgs){row, row_start, row_end, columns_count, massive, gen_value};
-                        pthread_create(&tid[i], NULL, func_for_threads, &args_array[i]);
-                        pthread_used++;
-                    }else{
-                        break;
-                    }
-                }
-                for (size_t i = 0; i < pthread_used; i++) {
-                        pthread_join(tid[i], NULL);
-                    }
-                rows_per_thread = 1;
-                remaining_rows = 0;
-                pthread_used = 0;
-                }
-            }
-        }
-    free(args_array);
-}
-
-void print_matrix(double** massive, size_t rows_count, size_t col_count){
-    for (size_t row = 0; row < rows_count; row++){
-        for (size_t col = 0; col < col_count; col++){
-            printf("%f ", massive[row][col]);
-        }
-            printf("\n");
-    }
-}
-
-void input_matrix(double** massive, size_t rows_count, size_t col_count){
-    for (size_t row = 0; row < rows_count; row++){
-        for (size_t col = 0; col < col_count; col++){
-            scanf("%lf", &massive[row][col]);
-        }
-    }
-}
-
-void generate_random_matrix(double** massive, size_t rows_count, size_t col_count) {
-    srand(time(NULL));
-    for (size_t row = 0; row < rows_count; row++) {
-        for (size_t col = 0; col < col_count; col++) {
-            massive[row][col] = (double)(rand() % 10); 
-        }
-    }
-}
-
-int main(int argc, char *argv[]){
-    size_t rows_count = 3;
-    size_t col_count = 4;
-    double**massive = malloc(sizeof(double *) * rows_count);
-    for (size_t line = 0; line < rows_count; line++){
-        massive[line] = malloc(col_count * sizeof(double));
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <array size> <number of threads>\n", argv[0]);
+        return 1;
     }
 
-    //generate_random_matrix(massive, rows_count, col_count);
-    input_matrix(massive, rows_count, col_count);
-    print_matrix(massive, rows_count, col_count);
-    const size_t thread_count = (size_t)atoi(argv[1]);
+    int arr_size = atoi(argv[1]);
+    int max_threads = atoi(argv[2]);
+
+    int *arr = (int *)malloc(arr_size * sizeof(int));
+    if (!arr) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+    for (int i = 0; i < arr_size; i++) {
+        arr[i] = rand() % 1000;
+    }
+
+    pthread_t threads[max_threads];
+    ThreadData threadData[max_threads];
+    int segment_size = arr_size / max_threads;
+
     clock_t start_time = clock();
-    Gauss_func(col_count, rows_count, massive, thread_count);
-    clock_t end_time = clock(); 
-    double elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
-    printf("Время выполнения Gauss_func: %f секунд\n", elapsed_time);
-    print_matrix(massive, rows_count, col_count);
-    for (size_t i = 0; i < rows_count; i++){
-        free(massive[i]);
+
+    for (int i = 0; i < max_threads; i++) {
+        threadData[i].arr = arr;
+        threadData[i].start = i * segment_size;
+        threadData[i].end = (i == max_threads - 1) ? arr_size : (i + 1) * segment_size;
+
+        pthread_create(&threads[i], NULL, sortArray, (void *)&threadData[i]);
     }
-    free(massive);
+
+    for (int i = 0; i < max_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    timSort(arr, arr_size);
+
+    clock_t end_time = clock();
+    double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    printf("Time taken for sorting with %d threads: %.5f seconds\n", max_threads, time_taken);
+
+    printf("Sorted Array: \n");
+    for (int i = 0; i < arr_size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    free(arr);
+    return 0;
 }
